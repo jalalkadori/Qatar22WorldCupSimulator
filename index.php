57acf44e7a5a7fe7cb54b198ed8792dfef0fbec5
Jalal KADDOURI <jalalkadori2@gmail.com>
2023-01-17 16:48:55 +0100

    Creating files and Adding page design

---
+++ index.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>World Cup Simulator</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container-fluid bg-body-tertiary">
+        <nav class="navbar ">
+            <div class="container d-flex">
+                <div>
+                    <a href="index.php">
+                        <img src="./images/logo.jpg" alt="Logo" width="70">
+                    </a>
+                </div>
+                
+                <div class="align-self-center">
+                    <h4>World cup Simulator</h4>
+                </div>
+            </div>
+        </nav>
+    </div>
+    
+    <section class="section">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col border border-danger ">
+                    <div class="row">
+                        <div class="col border d-flex align-items-center p-2 gap-2">
+                            <img src="./images/morocco.png" class="w-25 rounded-circle">
+                            <H3>Morocco</H3>
+                        </div>
+                        <div class="col">
+                            <h4 class="text-center">Match Score</h4>
+                            <div class="col border d-flex p-2 justify-content-center align-items-center">
+                                <input type="number" class="w-25" value="0">
+                                <input type="number" class="w-25" value="0">   
+                            </div>
+                        </div>
+                        
+                        <div class="col border d-flex justify-content-end align-items-center p-2 gap-2">
+                            <H3>Croatia</H3>
+                            <img src="./images/Croatia.png" class="w-25 rounded-circle">
+                        </div>
+                    </div>
+                </div>
+                <div class="col border border-warning">
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+</body>
+</html> 

9e3f11d48dcc1f1c3ff12d2171b7555dbed90e90
Jalal KADDOURI <jalalkadori2@gmail.com>
2023-01-18 11:14:18 +0100

    Adding Html content

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
                         <img src="./images/logo.jpg" alt="Logo" width="70">
                     </a>
                 </div>
-                
+                <button class="btn btn-danger">Simulate</button>
                 <div class="align-self-center">
                     <h4>World cup Simulator</h4>
                 </div>
@@ -28,28 +28,222 @@
             <div class="row">
                 <div class="col border">
                     <div class="row">
-                        <div class="col border d-flex align-items-center p-2 gap-2">
-                            <img src="./images/morocco.png" class="w-25 rounded-circle">
+                        <div class="col border d-flex align-items-center px-2 gap-2">
+                            <img src="./images/morocco.png" class="rounded-circle" style="width: 50px">
                             <H3>Morocco</H3>
                         </div>
                         <div class="col bg-secondary-subtle">
                             <h4 class="text-center">Match Score</h4>
-                            <div class="col border d-flex p-2 justify-content-center align-items-center">
+                            <div class="col d-flex px-2 justify-content-center align-items-center">
                                 <input type="number" class="w-25" value="0">
+                                <span class="fs-1">-</span>
                                 <input type="number" class="w-25" value="0">   
                             </div>
                         </div>
                         
-                        <div class="col border d-flex justify-content-end align-items-center p-2 gap-2">
+                        <div class="col border d-flex justify-content-end align-items-center px-2 gap-2">
                             <H3>Croatia</H3>
-                            <img src="./images/Croatia.png" class="w-25 rounded-circle">
+                            <img src="./images/Croatia.png" class="rounded-circle" style="width: 50px">
                         </div>
                     </div>
-                </div>
-                <div class="col border border-warning">
+
+                    <div class="row">
+                        
+                        <div class="col border d-flex align-items-center px-2 gap-2">
+                            <img src="./images/belgium.png" class="rounded-circle" style="width: 50px">
+                            <H3>Belguim</H3>
+                        </div>
+
+                        <div class="col bg-secondary-subtle">
+                            <h4 class="text-center">Match Score</h4>
+                            <div class="col d-flex justify-content-center align-items-center">
+                                <input type="number" class="w-25" value="0">
+                                <span class="fs-1">-</span> 
+                                <input type="number" class="w-25" value="0">   
+                            </div>
+                        </div>
+                        
+                        <div class="col border d-flex justify-content-end align-items-center px-2 gap-2">
+                            <H3>Canada</H3>
+                            <img src="./images/canada.jpg" class="rounded-circle" style="width: 50px">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        
+                        <div class="col border d-flex align-items-center px-2 gap-2">
+                            <img src="./images/belgium.png" class="rounded-circle" style="width: 50px">
+                            <H3>Belguim</H3>
+                        </div>
+
+                        <div class="col bg-secondary-subtle">
+                            <h4 class="text-center">Match Score</h4>
+                            <div class="col d-flex justify-content-center align-items-center">
+                                <input type="number" class="w-25" value="0">
+                                <span class="fs-1">-</span> 
+                                <input type="number" class="w-25" value="0">   
+                            </div>
+                        </div>
+                        
+                        <div class="col border d-flex justify-content-end align-items-center px-2 gap-2">
+                            <H3>Morocco</H3>
+                            <img src="./images/morocco.png" class="rounded-circle" style="width: 50px">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        
+                        <div class="col border d-flex align-items-center px-2 gap-2">
+                            <img src="./images/Croatia.png" class="rounded-circle" style="width: 50px">
+                            <H3>Croatia</H3>
+                        </div>
+
+                        <div class="col bg-secondary-subtle">
+                            <h4 class="text-center">Match Score</h4>
+                            <div class="col d-flex justify-content-center align-items-center">
+                                <input type="number" class="w-25" value="0">
+                                <span class="fs-1">-</span> 
+                                <input type="number" class="w-25" value="0">   
+                            </div>
+                        </div>
+                        
+                        <div class="col border d-flex justify-content-end align-items-center px-2 gap-2">
+                            <H3>Canada</H3>
+                            <img src="./images/canada.jpg" class="rounded-circle" style="width: 50px">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        
+                        <div class="col border d-flex align-items-center px-2 gap-2">
+                            <img src="./images/Croatia.png" class="rounded-circle" style="width: 50px">
+                            <H3>Croatia</H3>
+                        </div>
+
+                        <div class="col bg-secondary-subtle">
+                            <h4 class="text-center">Match Score</h4>
+                            <div class="col d-flex justify-content-center align-items-center">
+                                <input type="number" class="w-25" value="0">
+                                <span class="fs-1">-</span> 
+                                <input type="number" class="w-25" value="0">   
+                            </div>
+                        </div>
+                        
+                        <div class="col border d-flex justify-content-end align-items-center px-2 gap-2">
+                            <H3>Belgium</H3>
+                            <img src="./images/belgium.png" class="rounded-circle" style="width: 50px">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        
+                        <div class="col border d-flex align-items-center px-2 gap-2">
+                            <img src="./images/canada.jpg" class="rounded-circle" style="width: 50px">
+                            <H3>Canada</H3>
+                        </div>
+
+                        <div class="col bg-secondary-subtle">
+                            <h4 class="text-center">Match Score</h4>
+                            <div class="col d-flex justify-content-center align-items-center">
+                                <input type="number" class="w-25" value="0">
+                                <span class="fs-1">-</span> 
+                                <input type="number" class="w-25" value="0">   
+                            </div>
+                        </div>
+                        
+                        <div class="col border d-flex justify-content-end align-items-center px-2 gap-2">
+                            <H3>Morocco</H3>
+                            <img src="./images/morocco.png" class="rounded-circle" style="width: 50px">
+                        </div>
+                    </div>
+
                     
+
+
+                </div>
+
+                <div class="col broder border-warning">
+                    <table class="table table-striped text-center">
+                        <thead>
+                            <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Selection</th>
+                            <th scope="col">Pts</th>
+                            <th scope="col">GP</th>
+                            <th scope="col">W</th>
+                            <th scope="col">N</th>
+                            <th scope="col">L</th>
+                            <th scope="col">L</th>
+                            <th scope="col">L</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>
+                                    <img src="./images/morocco.png" class="rounded-circle" style="width: 80px">
+                                    <h3>Morocco</h3>
+                                </td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>
+                                    <img src="./images/Croatia.png" class="rounded-circle" style="width: 80px">
+                                    <h3>Croatia</h3>
+                                </td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>
+                                    <img src="./images/belgium.png" class="rounded-circle" style="width: 80px">
+                                    <h3>Belgium</h3>
+                                </td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row">4</th>
+                                <td>
+                                    <img src="./images/canada.jpg" class="rounded-circle" style="width: 80px">
+                                    <h3>Canada</h3>
+                                </td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
+
+
+            
         </div>
     </section>
   
